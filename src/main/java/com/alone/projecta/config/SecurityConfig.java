4467d906f09e2f3af63a8e66faec3b9a37066c9e
Alessandro Luiz <alessandroluizfacul@gmail.com>
2021-10-06 20:24:50 -0300

    Spring Security, implemented SecurityConfig class

---
+++ src/main/java/com/alone/projecta/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.alone.projecta.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+				.httpBasic();
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.inMemoryAuthentication()
+				.withUser("alessandro").password(encoder.encode("123")).roles("USER")
+				.and()
+				.withUser("admin").password(encoder.encode("123")).roles("USER", "ADMIN");
+	}
+}
